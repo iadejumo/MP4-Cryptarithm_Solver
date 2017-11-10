da574cf1b71e0d9d63b89555741eb5ba6a1c15d4
iadejumo <isaiahadejumo@gmail.com>
2017-11-10 15:22:13 -0800

    Modified Node contains method to search node on the right fixes #4

--- src/main/java/ca/ubc/ece/cpen221/mp4/avltree/AvlTreeSet.java
+++ src/main/java/ca/ubc/ece/cpen221/mp4/avltree/AvlTreeSet.java
@@ -218,6 +218,10 @@ public class AvlTreeSet {
             if (value < mValue) {
                 return mLeft.contains(value);
             }
+            
+            if (value > mValue) {
+                return mRight.contains(value);
+            }
 
             return false;
         }
