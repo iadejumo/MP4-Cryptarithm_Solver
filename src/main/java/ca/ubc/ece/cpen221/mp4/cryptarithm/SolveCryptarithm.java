9bd7f09b38e8bce6bce8b02bf69d8061ee9f8a57
iadejumo <isaiahadejumo@gmail.com>
2017-11-13 17:34:21 -0800

    Solve Main edited

--- src/main/java/ca/ubc/ece/cpen221/mp4/cryptarithm/SolveCryptarithm.java
+++ src/main/java/ca/ubc/ece/cpen221/mp4/cryptarithm/SolveCryptarithm.java
@@ -15,53 +15,12 @@ import ca.ubc.ece.cpen221.mp4.permutation.Permutation;
 
 public class SolveCryptarithm {
 
-	static public void main(String[] args) {
+	static public void main(String[] args) throws NoSolutionException {
 		// TODO implement this main method
+		Cryptarithm c = new Cryptarithm(args);
+		
+		System.out.print(c.solve());
+		
 	}
-	
-	public List<Map<Character, Integer>> getPossibleSolutions(Set<String> allLetters, Set<String> firstLetters){
-		List<Integer> allDigits = new ArrayList<Integer>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
-		
-		//Generate all permutations of the 10 digits
-		Permutation<Integer> permutation = new Permutation<Integer> (allDigits);
-		List<List<Integer>> allPermutations = permutation.getAllPermutations();
-		
-		List<String> lettersList = new ArrayList<String>();
-		lettersList.addAll(allLetters);
-		
-		//Get index of letter that appear first in the expressions
-		List<Integer> firstLetterIndices = new ArrayList<Integer>();
-		for (String letter: firstLetters) {
-			firstLetterIndices.add(lettersList.indexOf(letter));
-		}
-		
-		//Filter out all solution where any letter that appears first is equal to zero.
-		List<List<Integer>> nonZeroFirstPermutations;
-		for (Integer indexOfFirst: firstLetterIndices) {
-			nonZeroFirstPermutations =  allPermutations.parallelStream()
-																 .filter(p -> p.get(indexOfFirst) != 0)
-																 .collect(Collectors.toCollection(ArrayList::new));
-		}
-		
-		return null;
-	}
-	/*
-	private boolean evaluateSingleExpressions(Cryptarithm crypt, List<String> lettersList, List<Integer> assignmentsList) {
-		int numLetters = lettersList.size();
-		//Map<String, VariableExpression> copyOfMap = new HashMap<String, VariableExpression> ();
-	
-		
-		for(int index = 0; index < numLetters; index++) {
-			String letter = lettersList.get(index);
-			Integer value = assignmentsList.get(index);
-			crypt.mapOfUsedLetters.get(letter).store(value);
-		}
-		
-		double expression1 = crypt.exp1.eval();
-		double expression2 = crypt.exp2.eval();		
-		
-		return false;
-	}
-	*/
 	
 }
