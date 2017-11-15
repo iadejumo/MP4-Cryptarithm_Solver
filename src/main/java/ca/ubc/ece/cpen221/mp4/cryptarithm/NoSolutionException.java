b67c6f5b11998737df8ccd03deb1ff0e23cfe9f9
iadejumo <isaiahadejumo@gmail.com>
2017-11-15 12:11:40 -0800

    NoSolution Exception customized

--- src/main/java/ca/ubc/ece/cpen221/mp4/cryptarithm/NoSolutionException.java
+++ src/main/java/ca/ubc/ece/cpen221/mp4/cryptarithm/NoSolutionException.java
@@ -3,4 +3,7 @@ package ca.ubc.ece.cpen221.mp4.cryptarithm;
 public class NoSolutionException extends Exception {
 	// You can customize your exception class
 	// if you want
+	public NoSolutionException(){
+        System.out.println("No solution for the given Cryptarithm");
+    } 
 }
